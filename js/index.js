dba64bfb8ae5f13b5d4bcd1d24540e817f630c39
Ra0611 <an3611nt@gmail.com>
2021-05-17 21:50:24 +0300

    finished work

--- js/index.js
+++ js/index.js
@@ -1,33 +1,9 @@
 
+//! Получить и занести в новый ключ общее кол-во номеров в гостинице
+//! Расширить в инфо об апартаментах (добавить "наличие wifi", "кухня")
+//! * Добавить новый тип апартаментов (.push) 'luks'
 
-// 1 - без окончания
-// 2,3,4 - а
-// 5 - 20 - ов
-// 21, без окончания
-// 22, 23, 24 - а
-// 25 - 30 - ов
-// 31 - без окончания
-// 32-34 - а
-// 35-40 - ов 
-// 101 - номер
-//111-119 - дает ов
-//211-219 - дает ов
-//101
-const publicArea = {
-	key: '<th>Зоны общественного пользования</th>',
-	content: '<td>Общая кухня</td>'
-}
-const moreService = {
-	key: '<th>Разное</th>',
-	content: '<td>Номера для некурящих, Семейные номера, Звукоизолированные номера, Отопление, Курение на всей территории запрещено, Кондиционер</td>'
-}
-
-const luxNumbers = {
-	title: 'lux',
-	pay: 10000,
-	count: 11
-}
-
+//База данных
 const dataHotel = {
 	services: [
 		{
@@ -55,33 +31,50 @@ const dataHotel = {
 			pay: 1000,
 			count: 9
 		}
-		// {
-		// 	title:
-		// 	pay: 
-		// 	count: 
-		// }
 	]
 }
 
+//Создаем два новых, полных объекта
+const publicArea = {
+	key: '<th>Зоны общественного пользования</th>',
+	content: '<td>Общая кухня</td>'
+}
+const moreService = {
+	key: '<th>Разное</th>',
+	content: '<td>Номера для некурящих, Семейные номера, Звукоизолированные номера, Отопление, Курение на всей территории запрещено, Кондиционер</td>'
+}
+
+//Создаем новый, пустой объект
+const luxNumbers = {}
+
+//Добавляем полные объекты
 dataHotel.services.push(publicArea)
 dataHotel.services.push(moreService)
 
-// dataHotel.apartaments[3].title
+//Добавляем пустой объект
+dataHotel.apartaments.push(luxNumbers)
 
-// console.log(dataHotel.apartaments[3].title)
+//наполняем пустой объект
+dataHotel.apartaments[3].title = 'lux'
+dataHotel.apartaments[3].pay = 15000
+dataHotel.apartaments[3].count = 4
 
-const dataService = dataHotel.services
+//Создаем функцию использующую объекты services для заполнения таблицы "Услуги"
 function innerRow(inner) {
 	let i = inner.slice(-1); i--
+	const dataService = dataHotel.services
 	let row = document.querySelector(`.${inner}`)
 	row.innerHTML = dataService[i].key + dataService[i].content
 	return row
 }
+//используя функцию - заполняем таблицу "Услуги"
 innerRow('row1')
 innerRow('row2')
 innerRow('row3')
 innerRow('row4')
 
+//Достаем из навигации элементы выпадающего списка
+//Подключаем к этим элементам счетчики из нашей базы данных
 const contentVip = document.querySelector('.item-vip')
 const countVip = dataHotel.apartaments[0].count
 
@@ -91,125 +84,37 @@ const countPremium = dataHotel.apartaments[1].count
 const contentStandard = document.querySelector('.item-standard')
 const countStandard = dataHotel.apartaments[2].count
 
-// const count = countVip + countPremium + countStandard
-const count = countVip + countPremium + countStandard
-if ((count % 100) >= 11 && (count % 100) <= 19) {
-	numberTransformSt = 'ов'
-} else if ((count % 10) === 1) {
-	numberTransformSt = '' 
-} else if (count > 5 && count < 20) {
-	numberTransformSt = 'ов'
-} else if ((count % 10) >=2 && (count % 10) <=4) {
-	numberTransformSt = 'a'
-} else {
-	numberTransformSt = 'ов'
+const contentLux = document.querySelector('.item-lux')
+const countLux = dataHotel.apartaments[3].count
+
+//Получаем общее количество номеров в гостинице
+const countAll = countVip + countPremium + countStandard + countLux
+
+//Определяем окончание слова "номер" в зависимости от значений в счетчиках
+function counting(countEl) {
+	let count = countEl
+	if ((count % 100) >= 11 && (count % 100) <= 19) {
+		numberTransformSt = 'ов'
+	} else if ((count % 10) === 1) {
+		numberTransformSt = ''
+	} else if (count > 5 && count < 20) {
+		numberTransformSt = 'ов'
+	} else if ((count % 10) >= 2 && (count % 10) <= 4) {
+		numberTransformSt = 'a'
+	} else {
+		numberTransformSt = 'ов'
+	}
+	//Соединяем слово "номер" с вычисленным окончанием
+	let textAmount = (`номер${numberTransformSt}`)
+	return textAmount
 }
-let textAmount = `номер${numberTransformSt}`
 
 const infoAboutNumber = document.getElementById('navbarDropdownMenuLink')
-infoAboutNumber.innerText = (`Свободные номера: ${count} номер${numberTransformSt}`)
-contentVip.innerText = (`VIP ${countVip}: номер${numberTransformSt}`)
-contentPremium.innerText = (`Premium ${countPremium}: номер${numberTransformSt}`)
-contentStandard.innerText = (`Standard ${countStandard}: номер${numberTransformSt}`)
 
+//Возвращаем полученную информацию обратно в список
+infoAboutNumber.innerText = (`Всего  ${counting(countAll)}: ${countAll}`)
+contentVip.innerText = (`VIP: ${countVip} ${counting(countVip)}`)
+contentPremium.innerText = (`Premium: ${countPremium} ${counting(countPremium)}`)
+contentStandard.innerText = (`Standard: ${countStandard} ${counting(countStandard)}`)
+contentLux.innerText = (`Lux: ${countLux} ${counting(countLux)}`)
 
-const date = Number(prompt('!!'))
-console.log(count)
-// Получить и занести в новый ключ общее кол-во номеров в гостинице
-// Расширить в инфо об апартаментах (добавить "наличие wifi", "кухня")
-// * Добавить новый тип апартаментов (.push) 'luks'
-
-// const date = Number(prompt('!!'))
-// const key = 'first'
-// const human = {
-// 	name: 'Vasya',
-// 	age: null,
-// 	education: ['School', 'University'],
-// 	skills: {
-// 		works: '',
-// 		personal: ''
-// 	}
-// }
-// //Проверить что если human имеет скилл пунктуальный в личных х-ках, 
-// // то добавить этот скилл в рабочие качества
-// human.skills.personal = 'punctual'
-// human.amountEd = human.education.length
-// human.age = date
-// if (human.skills.personal === 'punctual') {
-// 	human.skills.works = 'punctual'
-// }
-// console.log(human)
-
-// function transformWordEnd(word, count, sex) {
-// 	let endWord = null
-// 	let lastCount = count % 10
-
-// const endWord = {
-// 	empty: '',
-// 	first: '',
-// 	second: ''
-// }
-// 	endWord.third = 'Hello'
-
-// 	if (sex === 'men') {
-
-// 	} else (sex === 'women') {
-
-// 	}
-
-// 	if (count > 5 && count < 20) {
-// 		endWord = endWord.second
-// 	} else if ((count % 100) >= 11 && (count % 100) <= 19) {
-// 		endWord = endWord.second
-// 	} else if (lastCount === 1) {
-// 		endWord = endWord.empty
-// 	} else if (lastCount >= 2 && lastCount <= 4) {
-// 		endWord = endWord.first
-// 	} else {
-// 		endWord = endWord.second
-// 	}
-// 	const text = word + endWord
-// 	return text
-// }
-// console.log(transformWordEnd("коза", 200, 'women'))
-
-// const textSt = `номер${numberTransformSt}`
-// console.log(`В нашем отеле есть ${data.apartaments[0].count} ${transformWordEnd("номер", data.apartaments[0].count)} класса ${transformWordEnd("номер", data.apartaments[2].count)} и ${data.apartaments[2].count} ${textSt} класса ${data.apartaments[2].title}`)
-// //В нашем отеле есть * номеров класса "vip" и * номеров класса "standard"
-
-// const news = [
-// 	{
-// 		name: 'News 1',
-// 		description: 'Text1'
-// 	},
-// 	{
-// 		name: 'News 2',
-// 		description: 'Text2'
-// 	},
-// 	{
-// 		name: 'News 3',
-// 		description: 'Text3'
-// 	},
-// ]
-// const step1 = document.getElementById('step1')
-// const step2 = document.getElementById('step2')
-// const step3 = document.getElementById('step3')
-// const step4 = document.getElementById('step4')
-// const step5 = document.getElementById('step5')
-
-// const loading = prompt('Are you loading data? (Y/N)')
-
-// if (loading === 'Y') {
-// 	step1.innerHTML = borsh[0]
-// 	step1.style.color = 'red'
-// 	step2.innerHTML = borsh[1]
-// 	step2.style.padding = '20px'
-// 	step3.innerHTML = borsh[2]
-// 	step3.style.color = 'yellow'
-// 	step4.innerHTML = borsh[3]
-// 	step4.style.color = 'green'
-// 	step5.innerHTML = borsh[4]
-// 	step5.style.color = 'lightblue'
-// } else {
-// 	console.log('False')
-// }
